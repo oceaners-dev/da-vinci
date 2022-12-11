ff9707ff4494290720756c9beca7d5a603f6d0cc
Ferşat <fertagram@gmail.com>
2022-12-11 22:14:59 +0300

    feat(hooks): added `useInViewport` hook

--- src/hooks/useClickOutside.tsx
+++ src/hooks/useClickOutside.tsx
@@ -1,40 +1,39 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react';
 
-const DEFAULT_EVENTS = ['mousedown', 'touchstart']
+const DEFAULT_EVENTS = ['mousedown', 'touchstart'];
 
 export function useClickOutside<T extends HTMLElement = any>(
   handler: () => void,
   events?: string[] | null,
   nodes?: HTMLElement[],
 ) {
-  const ref = useRef<T>()
+  const ref = useRef<T>();
 
   useEffect(() => {
     const listener = (event: any) => {
-      const { target } = event ?? {}
+      const { target } = event ?? {};
       if (Array.isArray(nodes)) {
         const shouldIgnore =
           target?.hasAttribute('data-ignore-outside-clicks') ||
-          !document.body.contains(target)
+          !document.body.contains(target);
         const shouldTrigger = nodes.every(
           (node) => !!node && !node.contains(target),
-        )
-        shouldTrigger && !shouldIgnore && handler()
+        );
+        shouldTrigger && !shouldIgnore && handler();
       } else if (ref.current && !ref.current.contains(target)) {
-        handler()
+        handler();
       }
-    }
+    };
 
-    ;(events || DEFAULT_EVENTS).forEach((fn) =>
+    (events || DEFAULT_EVENTS).forEach((fn) =>
       document.addEventListener(fn, listener),
-    )
+    );
 
     return () => {
-      ;(events || DEFAULT_EVENTS).forEach((fn) =>
-        document.removeEventListener(fn, listener),
-      )
-    }
-  }, [ref, handler, nodes])
+      const a = events || DEFAULT_EVENTS;
+      a.forEach((fn) => document.removeEventListener(fn, listener));
+    };
+  }, [ref, handler, nodes]);
 
-  return ref
+  return ref;
 }

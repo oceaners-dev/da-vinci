f3bc7954c54fa857be9ca47f50b4648c850c9ca6
Ferşat <fertagram@gmail.com>
2022-12-22 03:45:33 +0300

    feat(blockquote): added `classNames` prop and `story` for `Blockquote`

--- src/components/blockquote/Blockquote.tsx
+++ src/components/blockquote/Blockquote.tsx
@@ -2,18 +2,48 @@ import React from 'react'
 
 export interface BlockquoteProps {
   author: string
+  classNames?: {
+    footer?: string
+    source?: string
+    wrapper?: string
+  }
   quote: string
   source?: string
 }
 
-const Blockquote: React.FC<BlockquoteProps> = ({ quote, author, source }) => {
+// TODO: add box mode (with image)
+
+const Blockquote: React.FC<BlockquoteProps> = ({
+  classNames = {
+    footer: '',
+    source: '',
+    wrapper: '',
+  },
+  quote,
+  author,
+  source,
+}) => {
   return (
-    <blockquote className="text-xl font-semibold leading-tight text-gray-800 border-l-4 border-gray-600 pl-4 my-8">
+    <blockquote
+      data-tag="wrapper"
+      className={
+        'text-xl font-semibold leading-tight text-gray-800 border-l-4 border-gray-600 pl-4 my-8 h-fit w-fit ' +
+        classNames.wrapper
+      }
+    >
       {quote}
-      <footer className="text-sm font-light mt-2 text-gray-600">
+      <footer
+        data-tag="footer"
+        className={'text-sm font-light mt-2 text-gray-600 ' + classNames.footer}
+      >
         - {author}
         {source && (
-          <span className="ml-2 text-sm font-light text-gray-600">
+          <span
+            data-tag="source"
+            className={
+              'ml-2 text-sm font-light text-gray-600 ' + classNames.source
+            }
+          >
             ({source})
           </span>
         )}
---
+++ src/stories/Blockquote.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { Blockquote } from '../components/blockquote/Blockquote'
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Typography/Blockquote',
+  component: Blockquote,
+  parameters: {
+    docs: {
+      description: {
+        component: 'Blockquote component',
+      },
+    },
+    LabelPlaceholder: {
+      description: {
+        component: 'Blockquote component',
+      },
+    },
+  },
+} as ComponentMeta<typeof Blockquote>
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Blockquote> = (args) => (
+  <Blockquote {...args} />
+)
+
+export const Default = Template.bind({})
+export const Bordered = Template.bind({})
+export const WithImage = Template.bind({})
+export const Color = Template.bind({})
+export const Size = Template.bind({})
+
+Default.args = {
+  author: 'Albus Dumbledore',
+  quote:
+    'People find it far easier to forgive others for being wrong than being right.',
+  source: 'https://parade.com/1254364/jessicasager/dumbledore-quotes/',
+}

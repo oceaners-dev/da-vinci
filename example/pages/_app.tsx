ce64d5a655939b237eed3bbb30a3c3e837f57f34
Ferşat <fertagram@gmail.com>
2022-12-22 03:58:08 +0300

    chore(gitignore): troubleshooting gitignore

--- example/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import 'tailwindcss/tailwind.css'
-import 'da-vinci-ui/dist/styles.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
